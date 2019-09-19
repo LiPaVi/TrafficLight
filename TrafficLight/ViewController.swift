e0e42e495942f6e3e54de4df54a51b931d0cb6bb
LPalacheva@sportmaster.ru <>
2019-09-19 11:41:03 +0300

    init

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Palacheva Lidiya Vitalevna on 19/09/2019.
+//  Copyright © 2019 Palacheva Lidiya Vitalevna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
